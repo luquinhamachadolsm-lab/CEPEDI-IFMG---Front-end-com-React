325a161990d0fec732c485277d0d627ad386d0cb
Lucas SM <luquinhamachadolsm@gmail.com>
2025-11-21 16:36:19 -0300

    adiciona a aula 19

---
+++ AtividadesEmAulaEImcompletas/aula19/script.js
@@ -0,0 +1,66 @@
+class Empresa {
+    #nome; #departamentos;
+    constructor(nome) {
+    this.#nome = nome;
+    this.#departamentos = [];
+    }
+    get nome () { return this.#nome; }
+    adicionarDepartamento(departamentos) {
+        this.#departamentos.push(departamentos)
+    }
+    listarDepartamentos(){
+        console.log(this.#departamentos)
+    }
+    
+}
+class Empregado {
+    #nome; #empresa
+    constructor(nome, empresa) {
+    this.#nome = nome;
+    this.#empresa = empresa;
+    }
+    info() { console.log(`${this.#nome} trabalha na empresa ${this.#empresa.nome}.`); }
+    }
+objetoEmpresa = new Empresa("IFMG");
+empregadoObjeto = new Empregado("Lucas", objetoEmpresa);
+empregadoObjeto.info();
+
+class Departamento {
+    #nome;
+    constructor(nome) { this.#nome = nome; }
+    get nome () { return this.#nome; }
+}
+objetoDepart = new Departamento("Manutenção")
+console.log(objetoDepart.nome)
+
+class Endereco {
+    #logradouro; #numero; #bairro; #cidade; #estado; #cep;
+    constructor(logradouro, numero, bairro, cidade, estado, cep){
+        this.#logradouro = logradouro
+        this.#numero = numero
+        this.#bairro = bairro
+        this.#cidade = cidade
+        this.#estado = estado
+        this.#cep = cep
+    }
+}
+
+class Pessoa {
+    #nome; #cpf; #endereco;
+    constructor(nome, cpf, endereco) {
+        this.#nome = nome
+        this.#cpf = cpf
+        this.#endereco = new Endereco(endereco)
+    }
+}
+
+/////
+
+objetoDepart1 = new Departamento("TI")
+objetoDepart2 = new Departamento("RH")
+objetoDepart3 = new Departamento("FINANÇAS")
+
+adicionarDepartamento(objetoDepart1)
+adicionarDepartamento(objetoDepart2)
+adicionarDepartamento(objetoDepart3)
+console.log(objetoEmpresa.departamentos)
