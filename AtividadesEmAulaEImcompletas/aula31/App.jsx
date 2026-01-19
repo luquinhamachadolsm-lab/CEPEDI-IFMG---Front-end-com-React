31c7e5b51288a0e38f4a353829f0b740ed30d861
Lucas SM <luquinhamachadolsm@gmail.com>
2026-01-19 17:00:05 -0300

    Adiciona aula 31

---
+++ AtividadesEmAulaEImcompletas/aula31/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
+
+  useEffect(() => {
+    setCalculation(() => count * 2);
+  }, [count]); // <- add the count variable here
+
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <p>Calculation: {calculation}</p>
+    </>
+  );
+}
+
+
+
+
+export default App
